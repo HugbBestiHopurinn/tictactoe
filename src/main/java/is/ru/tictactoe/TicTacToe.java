1418dc95764efd844242f56dee71b108c2325dd0
gudmundurarnason <gudmundurarn@gmail.com>
2016-10-25 14:59:31 +0000

    Testing interface

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,7 +1,12 @@
 package is.ru.tictactoe;
 
 public class TicTacToe {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}	
+
+    public String something() {
+        return "hello";
+    }
+
+    //public static void main(String[] args) {
+    //    System.out.println("Hello World");
+    //}
+}
---
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -0,0 +1,37 @@
+package is.ru.tictactoe;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+public class TicTacToeWeb implements SparkApplication {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        SparkApplication tictactoeweb = new TicTacToeWeb();
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+
+        tictactoeweb.init();
+    }
+
+    @Override
+    public void init() {
+        final TicTacToe tictactoe = new TicTacToe();
+        //post("/random", (req, res) -> chuck.random());
+        post("/something", (req, res) -> tictactoe.something());
+
+        //post("/id", (req, res) -> chuck.getById(req.queryParams("id")));
+        //post("/setName", (req, res) -> {
+        //        chuck.alterName(
+        //                        req.queryParams("firstName"),
+        //                        req.queryParams("lastName")
+        //                        );
+        //        res.status(200);
+        //        return res;
+        //    });
+    }
+
+}
