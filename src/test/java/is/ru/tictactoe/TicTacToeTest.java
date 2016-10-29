34723c78d1a51cec0dafa221357362709da4900b
gudrunj14 <gudrunj14@ru.is>
2016-10-29 15:48:52 +0000

    Declare winner function and test added

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -4,10 +4,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.Rule;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class TicTacToeTest {
 	@Test
 	public void testSomething() {
 		assertEquals("this is something", TicTacToe.something());
+	}
+
+	@Test
+	public void testDeclareWinner() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		TicTacToe ticTacToe = New TicTacToe();
+		ticTacToe.declareWinner();
+		assertEquals(outContent.ToString(), "WE HAVE A WINNER: X");
 	}
 }
