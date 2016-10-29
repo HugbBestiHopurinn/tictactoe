e5eb7b05df458b68a62660f1e946d196b2a36b68
gudrunj14 <gudrunj14@ru.is>
2016-10-29 16:30:15 +0000

    Declare winner function and test added

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -2,15 +2,18 @@ package is.ru.tictactoe;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.rules.ExpectedException;
 import org.junit.Rule;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class TicTacToeTest {
+	private TicTacToe ticTacToe;
+
 	@Before 
 	public void setUp() {
-		TicTacToe ticTacToe = new TicTacToe();
+		ticTacToe = new TicTacToe();
 	}
 
 	@Test
