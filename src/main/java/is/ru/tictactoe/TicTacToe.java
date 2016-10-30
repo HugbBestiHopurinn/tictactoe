5309d1f6853132b9cb41adcf3fbc83d4d95c7369
gudmundurarnason <gudmundurarn@gmail.com>
2016-10-30 18:02:02 +0000

    Added comments.

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -9,6 +9,13 @@ public class TicTacToe {
     Player playerOne;
     Player playerTwo;
 
+    // Inputs: array, playerOne.board or playerTwo.board
+    // Outputs: integer number which in binary looks like the player's array
+    // Summary:
+    // This takes each player's board (ignoring the other players placings)
+    // of the form [1, 0, 0, 0, 0, 0, 0, 0, 0], where 1 represents that the player
+    // has made a mark. We transform this into a sum, which is stored as the binary number 100000000 for the exampe above.
+    // This will then use & to figure out of this is one of the victory conditions.
     int binarySum(int board []) {
         int sum = 0;
         for (int i = 0; i < 9; i++) {
@@ -21,6 +28,10 @@ public class TicTacToe {
         return sum;
     }
 
+    // Inputs: integer, the results from the binarySum operation on the playerOne.board or playerTwo.board
+    // Outputs: boolean, whether or not the player's board contains a victory configuration.
+    // Summary:
+    // We check the binary sum and see if therein lies a victory combo, by using & operations.
     boolean check_for_victory(int sum) {
         int victory_combo [] = {0x1C0, 0x38, 0x92, 0x54, 0x7, 0x124, 0x49, 0x111};
         boolean win_status = false;
@@ -72,7 +83,7 @@ public class TicTacToe {
             json.put("CurrentPlayer", "PlayerOne");
             json.put("PlayerBoard", playerBoard.toString());
             json.put("HasWon", String.valueOf(winningStatus));
-            return json;
+            return json; // This will be our response to the client
         } else {
             playerTwo.makeMove(cell);
             JSONArray playerBoard = new JSONArray(playerTwo.board);
@@ -85,7 +96,8 @@ public class TicTacToe {
             json.put("CurrentPlayer", "PlayerTwo");
             json.put("PlayerBoard", playerBoard.toString());
             json.put("HasWon", String.valueOf(winningStatus));
-            return json;
+            return json; // This is our response to the client
+
         }
     }
 
