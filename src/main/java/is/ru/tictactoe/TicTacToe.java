aba70b2112ab8c5710d0b3c712fdd63cfcd38fbd
gudmundurarnason <gudmundurarn@gmail.com>
2016-10-30 19:40:58 +0000

    Restructuring main file per resdesign.

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,28 +1,27 @@
 package is.ru.tictactoe;
 
-import java.util.Scanner;
-
 public class TicTacToe {
-  private Board board;
-  private Scanner scanner = new Scanner(System.in);
 
-    public String make_move(String player, String cell) {
-        return "Server has received move information.";
-	}
+    String gameType;
+    Player playerOne;
+    Player playerTwo;
 
-    public static String something() {
-        return "this is something";
-    }
-
-    //public static void main(String[] args) {
-    //    System.out.println("Hello World");
-    //}
-
-    public void declareWinner (char winner) {
-        System.out.print("WE HAVE A WINNER: " + winner);
-    }
-
-    public void declareDraw() {
-        System.out.print("WE HAVE A DRAW!");
+    // Inputs: array, playerOne.board or playerTwo.board
+    // Outputs: integer number which in binary looks like the player's array
+    // Summary:
+    // This takes each player's board (ignoring the other players placings)
+    // of the form [1, 0, 0, 0, 0, 0, 0, 0, 0], where 1 represents that the player
+    // has made a mark. We transform this into a sum, which is stored as the binary number 100000000 for the exampe above.
+    // This will then use & to figure out of this is one of the victory conditions.
+    int binarySum(int board []) {
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            int shift = (i - 1 != -1) ? (i - 1) : 0;
+            sum += (2 << shift) * board[i];
+        }
+        if (board[0] == 1) {
+            sum--;
+        }
+        return sum;
     }
 }
