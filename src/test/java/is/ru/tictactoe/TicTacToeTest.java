9e699bcdcf5b5201dafe2edffb8b5b3f2451781c
thoriro15 <thoriro15@ru.is>
2016-10-30 20:18:06 +0000

    BinarySum tests added

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -9,5 +9,27 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class TicTacToeTest {
+	@Before
+	public void setUp() {
+		ticTacToe = new TicTacToe();
+	}
+	
+	@Test
+	public void testBinarySumIs256() {
+		int[] board = {0, 0, 0, 0, 0, 0, 0, 0, 1};
+		assertEquals(256, ticTacToe.binarySum(board));
+	}
 
+	@Test
+	public void testBinarySumIs1() {
+		int[] board = {1, 0, 0, 0, 0, 0, 0, 0, 0};
+		assertEquals(1, ticTacToe.binarySum(board));
+	}
+	
+	@Test
+	public void testCheckForVictory() {
+		int[] board = {1, 1, 1, 0, 0, 0, 0, 0, 0};
+		int sum = ticTacToe.binarySum(board);
+		assertEquals(true, ticTacToe.checkForVictory(sum));
+	}
 }
